2679f527e5767955cc5caa0bfd1b8878d128f50f
Jimmy-Z <jimmy.z.z@gmail.com>
2026-07-27 15:34:00 +0800

    trivial

--- src/dmap.rs
+++ src/dmap.rs
@@ -43,6 +43,7 @@ impl<D: AsRef<[u8]>> DMap<D> {
 pub struct DMapBuilder {
 	files: Vec<(Vec<u8>, Vec<u8>, u64)>,
 	lists: Vec<(Vec<Vec<u8>>, u64)>,
+	count_hint: usize,
 }
 
 impl DMapBuilder {
@@ -50,28 +51,36 @@ impl DMapBuilder {
 		Self {
 			files: Vec::new(),
 			lists: Vec::new(),
+			count_hint: 0,
 		}
 	}
 	pub fn add_file(&mut self, path: &str, prefix: &[u8], v: u64) -> Result<()> {
 		let t0 = Instant::now();
 		let file = std::fs::read(path)?;
-		eprintln!("loaded \"{}\", {} bytes, in {}ms", path, file.len(), t0.elapsed().as_secs_f32() * 1000f32);
+		eprintln!(
+			"loaded \"{}\", {} bytes, in {}ms",
+			path,
+			file.len(),
+			t0.elapsed().as_secs_f32() * 1000f32
+		);
+		self.count_hint += file.len() / MAX_FQDN_LEN;
 		self.files.push((file, prefix.to_vec(), v));
 		Ok(())
 	}
 
 	pub fn add_list(&mut self, list: impl IntoIterator<Item = impl AsRef<[u8]>>, v: u64) {
-		self.lists
-			.push((list.into_iter().map(|e| e.as_ref().to_vec()).collect(), v));
+		let list: Vec<_> = list.into_iter().map(|e| e.as_ref().to_vec()).collect();
+		self.count_hint += list.len();
+		self.lists.push((list, v));
 	}
 
-	pub fn build(self, count_hint: usize) -> Result<DMap<Vec<u8>>> {
+	pub fn build(self) -> Result<DMap<Vec<u8>>> {
 		let mut bytes = 0;
 		// we have to sort them, not storing them all in memory is HARD
 		// to prevent memory fragmentation from lots of String alloc
 		// we read them in whole and use &str instead
 		let t0 = Instant::now();
-		let mut kv: Vec<(&[u8], u64)> = Vec::with_capacity(count_hint);
+		let mut kv: Vec<(&[u8], u64)> = Vec::with_capacity(self.count_hint);
 		for (file, prefix, v) in self.files.iter() {
 			for mut line in file.split(is_newline) {
 				line = line.trim_ascii();
@@ -164,7 +173,7 @@ mod tests {
 		let mut b = DMapBuilder::new();
 		b.add_list(&[b"com"], 0);
 		b.add_list(&[b"example.com"], 1);
-		let m = b.build(0x10).unwrap();
+		let m = b.build().unwrap();
 
 		assert_eq!(m.get("com"), Some(0));
 		assert_eq!(m.get("net"), None);
@@ -179,6 +188,6 @@ mod tests {
 	fn test_build() {
 		let mut b = DMapBuilder::new();
 		b.add_file("lst/domainswild", b"*.", 0).unwrap();
-		let _ = b.build(0x10).unwrap();
+		let _ = b.build().unwrap();
 	}
 }
